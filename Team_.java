98eba2aba28c0f58646be28e43b8fe8895fab107
mfaidzya <fauzanbusuk@gmail.com>
2013-11-15 09:12:29 +0700

    Create Team_.java

---
+++ Team_.java
@@ -0,0 +1,15 @@
+package playerentities;
+ 
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+ 
+@Generated(value="EclipseLink-2.5.0.v20130507-rNA", date="2013-11-08T03:19:10")
+@StaticMetamodel(Team.class)
+public class Team_ {
+ 
+public static volatile SingularAttribute<Team, String> teamname;
+public static volatile SingularAttribute<Team, String> league;
+public static volatile SingularAttribute<Team, Integer> teamId;
+ 
+}
