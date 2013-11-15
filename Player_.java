92d5cb86f53efbcbc0f76bfe7a7d1096317aed91
mfaidzya <fauzanbusuk@gmail.com>
2013-11-15 09:11:35 +0700

    Create Player_.java

---
+++ Player_.java
@@ -0,0 +1,17 @@
+package playerentities;
+ 
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+ 
+@Generated(value="EclipseLink-2.5.0.v20130507-rNA", date="2013-11-08T03:19:10")
+@StaticMetamodel(Player.class)
+public class Player_ {
+ 
+public static volatile SingularAttribute<Player, Integer> id;
+public static volatile SingularAttribute<Player, String> lastname;
+public static volatile SingularAttribute<Player, String> firstname;
+public static volatile SingularAttribute<Player, String> lastspokenwords;
+public static volatile SingularAttribute<Player, Integer> jerseynumber;
+ 
+}
