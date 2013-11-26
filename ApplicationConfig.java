f47817a3249ccbbb6e4347cc22fd754c7cf301c8
mfaidzya <fauzanbusuk@gmail.com>
2013-11-26 08:35:16 +0700

    Create ApplicationConfig.java

---
+++ ApplicationConfig.java
@@ -0,0 +1,31 @@
+package com.playerentity.service;
+
+import java.util.Set;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ * @author ARDITTO
+ */
+@javax.ws.rs.ApplicationPath("webresources")
+public class ApplicationConfig extends Application {
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> resources = new java.util.HashSet<>();
+        addRestResourceClasses(resources);
+        return resources;
+    }
+
+    /**
+     * Do not modify addRestResourceClasses() method.
+     * It is automatically populated with
+     * all resources defined in the project.
+     * If required, comment out calling this method in getClasses().
+     */
+    private void addRestResourceClasses(Set<Class<?>> resources) {
+        resources.add(com.playerentity.service.PlayerFacadeREST.class);
+        resources.add(com.playerentity.service.TeamFacadeREST.class);
+    }
+    
+}
